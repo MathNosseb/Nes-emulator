148db866ad3338911a51f466be817236e370e23d
Mathéo Besson <126401359+MathNosseb@users.noreply.github.com>
2026-08-14 19:32:33 +0000

    ajouts d instrcutions

--- instructions.c
+++ instructions.c
@@ -9,7 +9,11 @@ int    execute_instruction(uint8_t instruction, nes_6502 *processor)
         processor->P &= 0b11110111; //reset bit D
         processor->PC++;
         break;
-    
+    case 0x78:
+        //https://www.nesdev.org/wiki/Instruction_reference#SEI
+        processor->P |= 0b00000100; //I passe a 1
+        processor->PC++;
+        break;
     default:
         printf("Instruction %X non reconnue\n", instruction);
         return (1);
