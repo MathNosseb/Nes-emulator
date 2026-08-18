5e0658f1c0a00ac80386fbdd47e7deb764e67137
Mathéo Besson <126401359+MathNosseb@users.noreply.github.com>
2026-08-18 13:29:48 +0000

    ajout d instructions et reparation

--- instructions.c
+++ instructions.c
@@ -12,7 +12,7 @@ void    recalculate_negativ(uint8_t value, nes_6502 *processor)
 {
     //si le 7eme bit est a 1
     //0b10000000 -> ca
-    if (value & 0b10000000 == 0b10000000)
+    if ((value & 0b10000000) == 0b10000000)
         processor->P |= 0b10000000;
     else
         processor->P &= 0b01111111;
@@ -37,7 +37,7 @@ int    execute_instruction(uint8_t instruction, nes_6502 *processor, char *filen
     case 0xA2:
         //https://www.nesdev.org/wiki/Instruction_reference#LDX
         
-        processor->X = processor->ram[low];
+        processor->X = low;
         recalculate_zero(processor->X, processor);
         recalculate_negativ(processor->X, processor);
         processor->PC+=2;
@@ -49,10 +49,35 @@ int    execute_instruction(uint8_t instruction, nes_6502 *processor, char *filen
         break;
     case 0xAD:
         //https://www.nesdev.org/wiki/Instruction_reference#LDA
-        processor->ram[low | (high << 8)] = processor->A;
+        processor->A = processor->ram[low | (high << 8)];
         recalculate_zero(processor->A, processor);
         recalculate_negativ(processor->A, processor);
         processor->PC += 3;
+        break;
+    case 0x10:
+        //https://www.nesdev.org/wiki/Instruction_reference#BPL
+        if ((processor->P & 0b10000000) == 0b00000000)
+        {
+            //si N est a 0
+            processor->PC += 2 + (int8_t)low;
+            printf("jump to %d\n", (int8_t)low);
+        }
+        else
+        {
+            processor->PC += 2;
+        }
+        break;
+    case 0xCA:
+        //https://www.nesdev.org/wiki/Instruction_reference#DEX
+        processor->X--;
+        recalculate_zero(processor->X, processor);
+        recalculate_negativ(processor->X, processor);
+        processor->PC++;
+        break;
+    case 0x9A:
+        //https://www.nesdev.org/wiki/Instruction_reference#TXS
+        processor->SP = processor->X;
+        processor->PC++;
         break;
     default:
         printf("Instruction %X non reconnue\n", instruction);
